315801afdafd0b7a29207074f32210b21cddf982
Mathias Ducancel <mathiasduc@gmail.com>
2016-09-30 12:21:40 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+var obj= {
+    operator: false,
+    str1: "",
+    str2: "",
+    computing: function(str1,str2){
+        return(parseInt(str1, 10) obj.operator parseInt(str2, 10));
+    }
+}
+
+$("button").click(function(){
+
+
+});
+
+
+
+
+
+
+obj.operator = + ;
+console.log(obj.computing("100","100"));
